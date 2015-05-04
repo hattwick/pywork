692d65ca41ecbd61eedc749bf79501b8bd1173d8
hattwick <phil@hattwick.com>
2015-05-03 23:49:03 -0400

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+__author__ = 'phil'
+
+# testing various functions in small form, some 2.x, some 3.x
+
+
+x=15
+
+if x>20:
+    print "high range"
+elif x>10:
+    print "moderate range"
+else:
+    print "low range"
