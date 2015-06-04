e40c6f69076d2ccf614893eb6dae8682dcd18f4c
hattwick <phil@hattwick.com>
2015-06-04 11:08:23 -0400

    test after upgrade

---
+++ effpython-test.py
@@ -0,0 +1,26 @@
+__author__ = 'phil'
+
+# Sample snippets from Harrison and Slatkin books
+
+
+print('Enter your word')
+word=input()
+print('Checking for match pattern ...ate')
+match=word.find('ate')
+if (match<0):
+    print('no match')
+else:
+    print ('match position ', match)
+
+print('/n Moving onto file check/n')
+
+devices = ['router','switch','load_balancer','firewall']
+
+for device in devices:
+    print(device)
+
+print ('done')
+
+# ['router','switch','load_balancer','firewall','WanOptimizer' ]
+# for device in devices:
+#     print (device)
