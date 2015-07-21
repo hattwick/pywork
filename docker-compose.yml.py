57309bd1759cefb43de1262c930a47a8a2d40d37
hattwick <phil@hattwick.com>
2015-07-21 14:49:24 -0400

    log v2

--- docker-compose.yml.py
+++ docker-compose.yml.py
@@ -36,3 +36,50 @@ worker:
 
 # VERSION 2
 
+rng1:
+    build: rng
+
+
+rng2:
+    build: rng
+
+rng3:
+    build: rng
+
+rng0:
+    image: jpetazzo/hamba
+    links:
+      - rng1
+      - rng2
+      - rng3
+    command: 80 rng1 80 rng2 80 rng3 80
+    ports:
+      -  "8001:80"
+
+hasher:
+    build: hasher
+    ports:
+      - "8002:80"
+
+webui:
+    build: webui
+    #links:
+    #  - redis
+    ports:
+      - "8000:80"
+    extra_hosts:
+      redis: 52.6.86.223
+   # volumes:
+   #   - "webui/files/:/files/"
+
+#redis:
+#    image: redis
+
+worker:
+    build: worker
+    links:
+      - rng
+      - hasher
+    extra_hosts:
+      redis: 52.6.86.223
+
