aeb1212b7742667c5d353f56d4a7758e9d64dc9b
hattwick <phil@hattwick.com>
2015-12-18 16:34:03 -0500

    initial

---
+++ hw4e.py
@@ -0,0 +1,20 @@
+# Homework for Python Web Data Course, Coursera, Dr. Chuck
+# Test for HW4e, XML data extract
+
+import urllib
+import xml.etree.ElementTree as ET
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode.xml?'
+
+while True:
+    address = raw_input('Enter location: ')
+    if len(address) < 1 break
+
+    url = serviceurl + urllib.urlencode({'sensor':'false','address': address})
+    print 'Retrieving ', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved ',len(data), ' characters'
+    print data
+    tree = ET.fromstring(data)
+
