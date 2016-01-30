b115c951638eb18eadb4e32caf42fdb442a69c0a
Hattwick <philhattwick@charter.net>
2016-01-30 14:58:29 -0500

    minor

--- json1.py
+++ json1.py
@@ -11,7 +11,7 @@ import logging
 from pprint import pprint
 
 # import file with all JSON objects
-with open('sensortable.json') as json_data:
+with open('sensorabr.json') as json_data:
     ping = json.load(json_data)
 
 app_a_sensors = [
