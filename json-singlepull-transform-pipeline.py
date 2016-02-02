2cbf116bbc5a2bfbcb2f04ef789b5e698517e5fb
Hattwick <philhattwick@charter.net>
2016-02-01 23:31:00 -0500

    pep8 improvements

--- json-singlepull-transform-pipeline.py
+++ json-singlepull-transform-pipeline.py
@@ -11,11 +11,11 @@ from pprint import pprint
 
 # Retrieve and print static sensor
 
-print('Test static single-sensor pipeline)
+print('Test static single-sensor pipeline')
 print('\nNative json response:\n')
-macrosensor=json.load(urllib2.urlopen('https://serviceURL'))
+macrosensor = json.load(urllib2.urlopen('https://serviceURL'))
 print macrosensor
-sensor=macrosensor['sensordata']
+sensor = macrosensor['sensordata']
 
 # Result contained the dictionary we wanted inside of another dictionary.  This extracts the dictionary we want.
 
@@ -25,16 +25,16 @@ print(sensor)
 
 # Modify sensor to new format
 
-dashboard_sensor={
-    'applicationIdentifier':sensor['probename'],
-    'applicationCodes':[
+dashboard_sensor = {
+    'applicationIdentifier': sensor['probename'],
+    'applicationCodes': [
         sensor['uptime'],
         sensor['downtime'],
         sensor['updownsince']
     ],
-    'name':sensor['parentdevicename'],
-    'status':'PASS'if sensor['statustext']=='Up'else'ERROR',
-    'messages':[
+    'name': sensor['parentdevicename'],
+    'status': 'PASS' if sensor['statustext'] == 'Up' else 'ERROR',
+    'messages': [
         sensor['sensortype'],
     ]
 }
@@ -42,18 +42,18 @@ dashboard_sensor={
 # End of sensor processing
 
 
-#Transformed sensor posting-Local feedback and file drop
+# Transformed sensor posting-Local feedback and file drop
 
 print('\nSensor reformatted for new dashboard:\n')
 pprint(dashboard_sensor)
 
-filetag=dashboard_sensor['applicationIdentifier']
+filetag = dashboard_sensor['applicationIdentifier']
 
-withopen('%s.json'%filetag,'wb')asjson_data:
-json_data.write(json.dumps(dashboard_sensor))
+with open('%s.json' %filetag, 'wb') as json_data:
+    json_data.write(json.dumps(dashboard_sensor))
 
 print('\nPosted json file:')
 print(filetag)
 
 
-#-30-
+# -30-
