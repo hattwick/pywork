9362f7488108874be8d7ef91f786ce3775cd26a3
Hattwick <philhattwick@charter.net>
2016-02-06 22:51:04 -0500

    new utility started

---
+++ data/clipscrape.py
@@ -0,0 +1,20 @@
+#! python3
+# Python3 utility to take clipboard contents and extract contact information
+
+import re
+import pyperclip
+
+
+
+# TODO phone number regex
+re.compile(r'''
+)
+
+# TODO email regex
+
+
+# import the clipboard
+
+# extract contact information
+
+# copy the contact information back to clipboard
