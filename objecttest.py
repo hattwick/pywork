1e0c6fb25eed305c3c6dc7f18f90b18d805c8666
Hattwick <philhattwick@charter.net>
2016-02-16 22:55:21 -0500

    object test

---
+++ objecttest.py
@@ -0,0 +1,17 @@
+# Example for slope computation using objects
+
+class Line(object):
+
+	def __init__(self,coordinate1,coordinate2):
+		self.coor1 = coordinate1
+		self.coor2 = coordinate2
+
+	def distance(self):
+		x1, y1 = self.coor1
+		x2, y2 = self.coor2
+		return ( (x2=x1)**2 + (y2-y1)**2 )**0.5
+
+	def slope(self):
+		x1, y1 = self.coor1
+		x2, y2 = self.coor2
+		return float((y2-y1))/(x2-x1)
