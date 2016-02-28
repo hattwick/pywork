3ceabe4f6b7ba27d2be36f892ca10851911ad326
Hattwick <philhattwick@charter.net>
2016-02-27 23:17:26 -0500

    server base

--- netserv.py
+++ netserv.py
@@ -13,4 +13,13 @@ sock.setsockopt(socket.SOL_SOCKET, sock.SO_REUSEADDR, 1)   # create socket
 sock.bind((host,port))
 sock.listen(5)
 
-
+client, addr = sock.accept()
+data = c.recv(size)
+if data:
+	f = open("inboundstorage.dat", '+w')     # save received data to file
+	print(addr[0], ' has connected')
+	f.write(addr[0])
+	f.write(":")
+	f.write(data.decode("utf-8"))
+	f.close()
+sock.close()
