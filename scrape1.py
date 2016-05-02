4e8b5c7d1006ff2d35251d5548ae8f8c91bf40df
Hattwick <philhattwick@charter.net>
2016-05-01 22:29:33 -0400

    new file

--- scrape1.py
+++ scrape1.py
@@ -1,2 +1,6 @@
 # scraping example modified from "Web Scraping with Python"
+# written for Python3
 
+from urllib.request import urlopen
+html - urlopen("http://pythonscraping.com/pages/page1.html")
+print(html.read())
