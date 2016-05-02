0f4a176589de5165647df80b7154ac5993f93c93
Hattwick <philhattwick@charter.net>
2016-05-01 22:32:23 -0400

    html read and print

--- scrape1.py
+++ scrape1.py
@@ -1,6 +1,9 @@
 # scraping example modified from "Web Scraping with Python"
 # written for Python3
 
+import urllib
+import request
+
 from urllib.request import urlopen
 html - urlopen("http://pythonscraping.com/pages/page1.html")
 print(html.read())
