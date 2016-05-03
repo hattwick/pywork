0579a7f157f3bfb0e0559c9b6cd4091aa32a0cbe
Hattwick <philhattwick@charter.net>
2016-05-02 23:05:19 -0400

    add beautifulsoup formatting

--- scrape1.py
+++ scrape1.py
@@ -3,7 +3,12 @@
 
 import urllib
 import request
-
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
 html = urlopen("http://pythonscraping.com/pages/page1.html")
 print(html.read())
+
+print('//////')
+bsObj = BeautifulSoup(html.read())
+print(bsObj.h1)
