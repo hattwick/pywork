3b50305aaf30283193c0bca3673f8c575f5680a5
Hattwick <philhattwick@charter.net>
2016-05-08 11:21:08 -0400

    parent-child statement

--- scrape1.py
+++ scrape1.py
@@ -1,17 +1,9 @@
 # scraping example modified from "Web Scraping with Python"
 # written for Python3
 
-import urllib
-import request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
-
-print('//////')
-bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
-
-
-
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html)
+print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
