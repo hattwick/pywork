3422ecbca42d88716ddccd18f9d42bf7a42cd4df
Hattwick <philhattwick@charter.net>
2016-05-16 23:02:58 -0400

    fix beautifulsoup error

--- scrape1.py
+++ scrape1.py
@@ -4,7 +4,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-self.soup = BeautifulSoup(html, 'html.parser')
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
-bsObj = BeautifulSoup(html)
+bsObj = BeautifulSoup(html, 'html.parser')
 print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
