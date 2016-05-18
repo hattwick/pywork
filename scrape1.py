65f552496e8a2cae5a32c9f850bd8e8ffa1fe52a
Hattwick <philhattwick@charter.net>
2016-05-17 21:32:46 -0400

    oscon add

--- scrape1.py
+++ scrape1.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 import request
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+#html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+html = urlopen("http://www.oscon.com/index.html")
 bsObj = BeautifulSoup(html, 'html.parser')
 print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
