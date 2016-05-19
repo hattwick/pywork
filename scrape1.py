5fa4a3075f0446dd03a5791beaf30d60c6efe36d
Hattwick <philhattwick@charter.net>
2016-05-18 22:23:09 -0400

    oscon add

--- scrape1.py
+++ scrape1.py
@@ -9,3 +9,4 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.oscon.com/index.html")
 bsObj = BeautifulSoup(html, 'html.parser')
 print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
+
