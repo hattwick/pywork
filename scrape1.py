0c1c3443842806a3c3511fab4c9837915aba91c0
Hattwick <philhattwick@charter.net>
2016-05-19 21:29:52 -0400

    normalized

--- scrape1.py
+++ scrape1.py
@@ -5,8 +5,8 @@ from urllib.request import urlopen
 import request
 from bs4 import BeautifulSoup
 
-#html = urlopen("http://www.pythonscraping.com/pages/page3.html")
-html = urlopen("http://www.oscon.com/index.html")
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+# html = urlopen("http://www.oscon.com/index.html")
 bsObj = BeautifulSoup(html, 'html.parser')
 print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
 
