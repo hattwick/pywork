5cb7d583460f0ee6165fcac213ff01607ec91beb
Hattwick <philhattwick@charter.net>
2016-07-06 21:48:44 -0400

    newtest

---
+++ pydatatest.py
@@ -0,0 +1,7 @@
+# Test of router list and element manipulation
+
+vendors = ['Juniper', 'Cisco', 'Arista', 'Citrix', 'NGINX', 'Timetra']
+
+switches = ['N5K', 'N9K', 'N7K','N3K', '4507','6500','7050X']
+
+print('/nVENDORS/n')
