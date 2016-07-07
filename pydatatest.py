de504f9cb49a783ced18293b3bba81115894cce8
Hattwick <philhattwick@charter.net>
2016-07-06 21:56:25 -0400

    newtest

--- pydatatest.py
+++ pydatatest.py
@@ -1,7 +1,15 @@
 # Test of router list and element manipulation
+# Using Python3
 
 vendors = ['Juniper', 'Cisco', 'Arista', 'Citrix', 'NGINX', 'Timetra']
 
 switches = ['N5K', 'N9K', 'N7K','N3K', '4507','6500','7050X']
 
-print('/nVENDORS/n')
+print('\nVENDORS - no list manipulation\n')
+print(vendors)
+
+print('\nVENDORS - all members\n')
+print(vendors[0:])
+
+print('\nVENDORS - print just the last member (syntax disappears)\n')
+print(vendors[-1])
