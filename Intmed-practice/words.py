1c554830d6f7018539bca6705ebbceeb74423dbf
Hattwick <philhattwick@charter.net>
2016-08-23 22:53:24 -0400

    print for debug

--- Intmed-practice/words.py
+++ Intmed-practice/words.py
@@ -12,6 +12,10 @@ scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
           "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
           "x": 8, "z": 10}
 
+print('\nWORDLIST\n')
 print(wordlist)
+print('\nSCORES\n')
+print(scores)
+
 
 # -30-
