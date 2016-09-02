36c20e9ba229b11d6f80563b67b1b93a16c2e212
Hattwick <philhattwick@charter.net>
2016-09-01 21:49:32 -0400

    pygame test from book

---
+++ Intmed-practice/gametest.py
@@ -0,0 +1,22 @@
+# Testing code from Python Crash Course
+
+import sys
+import pygame
+
+def run_game():
+	# Initialize game and create a screen object.
+	pygame.init()
+	screen = pygame.display.set_mode((1200, 800))
+	pygame.display.set_caption("Alien Invasion")
+
+	# Start the main loop for the game.
+	while True:
+		# Watch for keyboard and mouse events.
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+		# Make the most recently drawn screen visible.
+		pygame.display.flip()
+
+run_game()    #ERROR: NameError: name 'pygame' is not defined
