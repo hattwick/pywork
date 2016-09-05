23a2e1c24325186646b7cb239af9da73ed04e7b5
Hattwick <philhattwick@charter.net>
2016-09-05 13:27:07 -0400

    timestamp added

--- Intmed-practice/words.py
+++ Intmed-practice/words.py
@@ -1,5 +1,7 @@
 # Practice Exercise from McKellar Intermediate Python
 
+from datetime import datetime
+
 WORD_LIST = "sowpods.txt"
 wordlist = open(WORD_LIST).readlines()
 # Get rid of newlines
@@ -11,6 +13,9 @@ scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
           "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
           "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
           "x": 8, "z": 10}
+
+currtime = datetime.now()
+print ("Current Time: ", currtime.strftime("%x, %X"))
 
 print('\nWORDLIST\n')
 print(wordlist)
