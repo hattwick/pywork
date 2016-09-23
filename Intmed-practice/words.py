090e9dec96ea998d3ba400c3f020daf4bc0fa62d
Hattwick <philhattwick@charter.net>
2016-09-22 23:57:45 -0400

    pep 8

--- Intmed-practice/words.py
+++ Intmed-practice/words.py
@@ -8,6 +8,7 @@ import sys
 import os
 
 WORD_LIST = "sowpods.txt"
+
 # Get rid of newlines
 wordlist = open(WORD_LIST).readlines()
 wordlist = [word.lower().strip() for word in wordlist]
@@ -17,6 +18,7 @@ scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
           "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
           "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
           "x": 8, "z": 10}
+
 
 # Print environmentals
 currtime = datetime.now()
