cd46fb238aad2bd107faab0a61eb305683207aaf
Hattwick <philhattwick@charter.net>
2016-10-11 21:42:32 -0400

    clear old files

---
+++ helper1.py
@@ -0,0 +1,11 @@
+# Helper file to main1.py
+
+def hello(name):
+	print('Hello,', name)
+
+
+def main():
+	print('Main from Moves has been executed.  How did that happen?')
+	hello("Nick")
+
+if __name__ == "__main__": main()
---
+++ main1.py
@@ -0,0 +1,6 @@
+# Simple test for importing functions from other files
+
+from helper1 import hello
+
+print('This is the main1 program running here...\n')
+hello("nick")
