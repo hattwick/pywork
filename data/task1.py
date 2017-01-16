85fe2622745be8afd91e75f7a3e323ea2379e410
Hattwick <philhattwick@charter.net>
2017-01-15 20:32:45 -0500

    minor debug plus test instruction

--- data/task1.py
+++ data/task1.py
@@ -8,11 +8,19 @@ def get_stock_info(stock, start, end, source='yahoo'):
 	df['Stock'] = stock
 	agg = df.groupby('Stock').agg({
 		'Open': ['min','max','mean','median'],
-		'Adjusted Close': ['min','max','mean','median'],
+		'Adjust Close': ['min','max','mean','median'],
 		'Close': ['min','max','mean','median'],
 		'High': ['min','max','mean','median'],
 		'Low': ['min','max','mean','median'],
 	})
-	agg.columns = [' '.join(col).strip() for col in agg.columns.values]
-	return agg.to_json()
+	agg.columns = [' '.join(col).strip() for col in agg.columns.values]  #readability
+	return agg.to_json()  #return dataframe
 
+# -30- End of task
+
+'''Can test in REPL with
+from task1 import get_stock_info
+from datetime import dateime
+res = get_stock_info('__ticker_symbol__', datetime(2017, 1, 1), datetime.today())
+res
+'''
