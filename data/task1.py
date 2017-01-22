b1dd31733176d98636088d28d78677af9361ac00
Hattwick <philhattwick@charter.net>
2017-01-22 14:41:57 -0500

    code inspection completed

--- data/task1.py
+++ data/task1.py
@@ -6,18 +6,20 @@
 from pandas_datareader import data
 from datetime import datetime
 
+
 def get_stock_info(stock, start, end, source='yahoo'):
-	df = data.DataReader(stock, source, start,end)
+	df = data.DataReader(stock, source, start, end)
 	df['Stock'] = stock
 	agg = df.groupby('Stock').agg({
-		'Open': ['min','max','mean','median'],
-		'Adj Close': ['min','max','mean','median'],
-		'Close': ['min','max','mean','median'],
-		'High': ['min','max','mean','median'],
-		'Low': ['min','max','mean','median'],
+		'Open': ['min', 'max', 'mean', 'median'],
+		'Adj Close': ['min', 'max', 'mean', 'median'],
+		'Close': ['min', 'max', 'mean', 'median'],
+		'High': ['min', 'max', 'mean', 'median'],
+		'Low': ['min', 'max', 'mean', 'median'],
 	})
-	agg.columns = [' '.join(col).strip() for col in agg.columns.values]  #readability
-	return agg.to_json()  #return dataframe
+	agg.columns = [' '.join(col).strip() for col in agg.columns.values]  # readability
+	return agg.to_json()  # return dataframe
+
 
 # -30- End of task
 
@@ -29,8 +31,10 @@ res = get_stock_info('__ticker_symbol__', datetime(2017, 1, 1), datetime.today()
 res
 '''
 
+
 def main():
 	result = get_stock_info('ANET', datetime(2016, 1, 1), datetime.today())
 	print(result)
+
 
 if __name__ == "__main__": main()
